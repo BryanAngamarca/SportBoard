be9eb463010b1ba32a541a32a3443d0b1793766c
BryanAngamrca <bryan.angamarca@unl.edu.ec>
2024-10-29 22:08:09 -0500

    Auto stash before merge of "develop" and "origin/develop"

--- frontend/frontend/src/app/tiempo-real/cronologia/cronologia.component.ts
+++ frontend/frontend/src/app/tiempo-real/cronologia/cronologia.component.ts
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import { Component, inject } from '@angular/core';
+=======
+import { Component, OnInit, OnDestroy } from '@angular/core';
+>>>>>>> Stashed changes
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TrEstadisticaComponent } from '../tr-estadistica/tr-estadistica.component';
@@ -12,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './cronologia.component.html',
   styleUrl: './cronologia.component.scss'
 })
+<<<<<<< Updated upstream
 export class CronologiaComponent {
   dialog = inject(MatDialog);
 
@@ -41,4 +46,39 @@ export class CronologiaComponent {
   imports: [MatCardModule, MatButtonModule],
 })
 export class ejem {
+=======
+export class CronologiaComponent implements OnInit, OnDestroy {
+  time: string = '00:00'; // Variable que contendrá el tiempo
+  private intervalId: any;
+
+  ngOnInit() {
+    this.startTimer(); // Iniciar el reloj cuando el componente se cargue
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId); // Limpiar el intervalo cuando el componente se destruya
+    }
+  }
+
+  startTimer() {
+    let minutes = 0;
+    let seconds = 0;
+
+    this.intervalId = setInterval(() => {
+      seconds++;
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+      this.time = this.formatTime(minutes, seconds);
+    }, 1000); // Actualizar cada segundo
+  }
+
+  formatTime(minutes: number, seconds: number): string {
+    const min = minutes < 10 ? `0${minutes}` : minutes;
+    const sec = seconds < 10 ? `0${seconds}` : seconds;
+    return `${min}:${sec}`;
+  }
+>>>>>>> Stashed changes
 }
